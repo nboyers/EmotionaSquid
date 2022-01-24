cd9586804a04210c70830194bfc3203affd55b46
nboyers <frenttv@gmail.com>
2022-01-23 21:41:40 -0500

    Initial Commit

---
+++ EmotionalSquid/EmotionalSquidApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmotionalSquidApp.swift
+//  EmotionalSquid
+//
+//  Created by Noah Boyers on 1/23/22.
+//
+
+import SwiftUI
+
+@main
+struct EmotionalSquidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
